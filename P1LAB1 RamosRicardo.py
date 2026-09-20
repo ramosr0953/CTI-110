4e42aca28e25356bdcb6e654d9289002988f7dcc
ramosr0953 <ramosr0953@student.faytechcc.edu>
2026-09-20 19:35:04 -0400

    Add files via upload

---
+++ P1LAB1 RamosRicardo.py
@@ -0,0 +1,9 @@
+# Ricardo Ramos
+# September 20, 2026
+# P1LAB1
+# Creating a Python Program by getting input from the user and displaying the output
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")  
+
+print("Hello, ",first_name, last_name +"!", "Welcome to CTI-110!")
